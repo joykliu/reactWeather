c414337a275bbf0358c8a491a30a9c75decd6b61
Joy Liu <liukang.joy@gmail.com>
2016-09-18 16:08:04 -0400

    on lesson 12, section challenge

--- public/app.jsx
+++ public/app.jsx
@@ -1,23 +1,68 @@
-// nameing convention in react - fistletter uppercase then camel casing
+var GreeterMessage = React.createClass({
+    // NOTE: a presentational component: takes propes and renders them on to the screen
+    render: function(){
+        var name = this.props.name;
+        var message = this.props.message;
+        return(
+            <div>
+                <h1>hello {name}!</h1>
+                <p>{message}</p>
+            </div>
+        );
+    }
+});
+
+var GreeterForm = React.createClass({
+    // NOTE: a presentational component: takes propes and renders them on to the screen
+    onButtonClick: function(e){
+        e.preventDefault();
+        var name = this.refs.name.value;
+        if(name.length > 0) {
+            this.refs.name.value = '';
+            this.props.onNewName(name);
+            //name value passed into onNewName, and gets passed to handleNewName by onNewName
+        }
+    },
+
+    render: function(){
+        return(
+            <form onSubmit={this.onButtonClick}>
+                <input type="text" ref="name"/>
+                <button>click me</button>
+            </form>
+        )
+    }
+})
+
 var Greeter = React.createClass({
-    getDefaultProps: function() {
+    // NOTE: A container component. maintains the state of the application, and when the state gets update it updates its children.
+    getDefaultProps: function() {// native react thing, it's an anonymous function
         return {
             name: 'React',
-            message: 'no message for you'
-        };// default props only work if no attribute componet is present 
+            message: 'This is the default message'
+        };// default props only work if no attribute component is present
+        // this whole thing looks super similar to handlebar.js
     }, // will render default prop IF no prop called name is specified. Will render prop called name if it is specified
-    render: function() { // defining the render method as anonymous funciton that doesn't take in any arguments
-        // return React.createElement(
-        //     'h1',
-        //     null,
-        //     'Hello React.createElement!'
-        // ) //instead of using JSX code, we call react.createElement which results in longer, harder to read
-        var name = this.props.name;
+    getInitialState: function(){
+        return {
+            name: this.props.name,
+            message: this.props.message
+        };
+    },
+    handleNewName: function(name){//function to be passed on in greeterForm
+        this.setState({
+            name: name
+        });
+    },// every funciton inside the component is seperated by a comma
+    render: function() { // defining the render method property as anonymous function that doesn't take in any arguments
+        var name = this.state.name;//accessting name defined n getInitalState
         var message = this.props.message; // defines an variable that contains the value of the component's name prop value
         return ( // when we return jsx, we can only return ONE ROOT element.
             <div>
-                <h1>hello {name}</h1>
-                <p>{message + '!!'}</p>
+                <GreeterMessage name={name} message={message}/>
+                <GreeterForm onNewName={this.handleNewName}/>
+                {/* calls handleNewName in onNewName. onNewName gets
+                called in GreeterForm */}
             </div>
         );// () are not required syntax thing. It just helps organize the code
     }
@@ -25,8 +70,19 @@ var Greeter = React.createClass({
 
 var firstName = 'Joy'; // defines variable to use insite curly brackets
 // var myMessage = 'got a message for you all';
+var myMessage = 'i got a message for you';
 
-ReactDOM.render( // calling reactdom.reder
-    <Greeter name={firstName} />, // renders the component to run, use the tag name created up there. passing a name attribute to serve as a prop
+ReactDOM.render( // calling reactdom.reder, most common react DOM method
+    <Greeter name={firstName} message={myMessage}/>, // renders the component to run, use the tag name created up there. passing a name attribute to serve as a prop
     document.getElementById('app')// passing in element id aka location to reder the first argument
 );
+
+
+
+/****** NOTE: First props get defined. it either runs based on the value passed in
+****** or falls back to default
+****** getInitalState sets the name state to the prop's name value
+****** in render funciton this.state.name fetches the name's value
+****** RETURNS the name's value to display in <h1>
+****** when user submits form
+****** this.setState re-renders part of the component defined in .setState */
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 
 // Creat our app
+// calling thie express library
 var app = express();
-
+// express static sepcify a foldername
 app.use(express.static('public'));
 
+//.listen takes two arguments, one is the port, the other one is a funciton
 app.listen(3000, function(){
     console.log('express server is up on port 3000!')
 });
+
+// =====> WEB SERVER IS ESSENTIAL TO RUN REACT
