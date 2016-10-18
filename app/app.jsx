a2082a1e1fe9190eaf30afefde5dd0e4b12b8fda
Joy Liu <liukang.joy@gmail.com>
2016-10-18 07:49:35 -0400

    add foundation and jqeury

--- app/app.jsx
+++ app/app.jsx
@@ -1,20 +1,39 @@
-// LOADING REACT
-var React = require('react');
-var ReactDOM = require('react-dom');
+//*************************************************
+//     L O A D I N G   R E A C T
+//*************************************************
+const React = require('react');
+const ReactDOM = require('react-dom');
 
-// LOADING MORE REACT COMPONENTS USING ES6 destructuring
-// works the same as below, this is ES6 object destructuring
-//var Route = require('react-router').Route
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+//*************************************************
+//     L O A D I N G  R O U T I N G
+//*************************************************
+// Using ES6 destrucuring
+// works the same as below
+//const Route = require('react-router').Route
+const {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
-//LOADING COMPONENTS BY CALLING ALIASES
-var Main = require('Main');
-var Nav = require('Nav');
-var Weather = require('Weather');
-var About = require('About');
-var Examples = require('Examples');
 
-ReactDOM.render( // calling reactdom.reder, most common react DOM method
+//*************************************************
+//     L O A D I N G  C O M P O N E N T S
+//*************************************************
+const Main = require('Main');
+const Nav = require('Nav');
+const Weather = require('Weather');
+const About = require('About');
+const Examples = require('Examples');
+
+//*************************************************
+//     L O A D I N G  F O U N D A T I O N
+//*************************************************
+// use css! to package-fy css file
+// use style! to chain css file to html
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
+//*************************************************
+//     A P P  R E N D E R I N G
+//*************************************************
+ReactDOM.render(
     <Router history={hashHistory}>
     {/*Uses history prop with a `hashHistory` to use the `#/` combination to maintain the routes for the app`*/}
         <Route path="/" component={Main}>
@@ -28,5 +47,5 @@ ReactDOM.render( // calling reactdom.reder, most common react DOM method
                 use `IndexLink` instead of `Link`*/}
         </Route>
     </Router>, // renders the component to run, use the tag name created up there. passing a name attribute to serve as a prop
-    document.getElementById('app')// passing in element id aka location to reder the first argument
+    document.getElementById('app')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
+const webpack = require('webpack');
+
 module.exports = {
-    entry: './app/app.jsx',
+    //*************************************************
+    //     E N T R Y   P O I N T S
+    //*************************************************
+    entry: [
+        // use `script!` to make script files pacakges for webpack
+        'script!jquery/dist/jquery.min.js',
+        'script!foundation-sites/dist/foundation.min.js',
+        './app/app.jsx'
+    ],
+    externals: {
+        jquery: 'jQuery'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            // takes in one key value arguemet
+            '$': 'jquery',
+            'jQuery': 'jquery'
+        })
+    ],
     output: {
         path: __dirname,
         filename: './public/bundle.js'
     },
+    //*************************************************
+    //   T O   B E  R E S O L V E D  F I L E S
+    //*************************************************
     resolve: {
         root: __dirname,//variable available in node.js by default
         alias: {
