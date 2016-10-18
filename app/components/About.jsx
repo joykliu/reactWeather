7286b929916e38f15cfe09e34d271b7df5114302
Joy Liu <liukang.joy@gmail.com>
2016-10-17 22:56:54 -0400

    changed message on examples and about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,6 +1,11 @@
 const React = require('react');
 /* only use this when only render a props
 ** and doesn't maintain any state */
-const About = props => (<h3> about component</h3>)
+const About = props => (
+    <div class="wraper">
+        <h3>About</h3>
+        <p>Welcome to About Page</p>
+    </div>
+)
 
 module.exports = About;
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,10 @@
 const React = require('react')
 
-const Examples = props => (<h3>Examples</h3>)
+const Examples = props => (
+    <div class="wrapper">
+        <h3>Examples</h3>
+        <p>Welcome to Examples Page</p>
+    </div>
+)
 
 module.exports = Examples;
