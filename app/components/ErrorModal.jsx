52aeda72103dc978d80ea11bc52a1edf8e00e7d5
Joy Liu <liukang.joy@gmail.com>
2016-10-20 00:11:36 -0400

    added errorModal component

---
+++ app/components/ErrorModal.jsx
@@ -0,0 +1,27 @@
+const React = require('react');
+const Weather = require('Weather');
+
+const ErrorModal = React.createClass({
+    /* NOTE: componentDidMount method allows DOM manipualtion
+    ** after all the components have been rendered */
+
+    componentDidMount: function() {
+        // create new instance of foundation.reveal
+        // accepts jquery selector or `refs`
+        var modal = new Foundation.Reveal($('#error-modal'));
+        modal.open();
+    },
+    render: function(){
+        return(
+            <div id="error-modal" className="reveal tiny text-center" data-reveal>
+                <h4>Some title</h4>
+                <p>Our error message</p>
+                <button className="button hollow" data-close>
+                    FINE!
+                </button>
+            </div>
+        )
+    }
+})
+
+module.exports = ErrorModal
